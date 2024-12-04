566bf6b5fe822c7f3f60d4d94bcfead3e8aa389d
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-04 13:48:15 +0400

    add dbsets for products, categories, tags, tagproducts, and productimages

---
+++ NestFrontToBack/DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NestFrontToBack.Models;
+
+namespace NestFrontToBack.DAL;
+
+public class AppDbContext : IdentityDbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<Tag> Tags { get; set; }
+
+    public DbSet<TagProduct> TagProducts { get; set; }
+
+    public DbSet<ProductImage> ProductImages { get; set; }
+}
